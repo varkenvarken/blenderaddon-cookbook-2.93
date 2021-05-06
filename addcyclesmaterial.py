4e26936e51ff9208b439bbcac9b3dab2eaa3da59
varkenvarken <michel@michelanders.nl>
2021-05-06 14:54:10 +0200

    update for cycles + eevee

--- addcyclesmaterial.py
+++ addcyclesmaterial.py
@@ -21,33 +21,34 @@
 import bpy
 
 bl_info = {
-	"name": "Add Cycles Material",
+	"name": "Add Material",
 	"author": "Michel Anders (varkenvarken)",
-	"version": (0, 0, 202104291042),
+	"version": (0, 0, 202105061320),
 	"blender": (2, 92, 0),
-	"location": "Node > Add > Add Cycles Material",
-	"description": "A a basic cycles material",
+	"location": "Node > Add > Add aterial",
+	"description": "Add a basic node based material",
 	"warning": "",
 	"wiki_url": "",
 	"tracker_url": "",
 	"category": "Experimental development"}
 
 
-class AddCyclesMat(bpy.types.Operator):
-	bl_idname = 'mode.add_cycles_material'
-	bl_label = 'Add Cycles Material'
+class AddNodeMat(bpy.types.Operator):
+	bl_idname = 'node.add_material'
+	bl_label = 'Add Material'
 	bl_options = {'REGISTER', 'UNDO'}
 
 	@classmethod
 	def poll(self, context):
 		return ((context.active_object is not None )
-			and (context.scene.render.engine == 'CYCLES'))
+				and (context.space_data.type == 'NODE_EDITOR')
+				and (context.space_data.shader_type == 'OBJECT'))
 
 	def execute(self, context):
 		bpy.ops.object.material_slot_add()
 		ob = context.active_object
 		slot = ob.material_slots[ob.active_material_index]
-		mat = bpy.data.materials.new('Cycles material')
+		mat = bpy.data.materials.new('Material')
 		mat.use_nodes = True
 		slot.material = mat
 		return {"FINISHED"}
@@ -62,11 +63,11 @@ def menu_func(self, context):
 
 def menu_func(self, context):
 	self.layout.operator(
-		AddCyclesMat.bl_idname,
-		text=AddCyclesMat.bl_label,
+		AddNodeMat.bl_idname,
+		text=AddNodeMat.bl_label,
 		icon='PLUGIN')
 
-classes = [AddCyclesMat]
+classes = [AddNodeMat]
 
 register_classes, unregister_classes = bpy.utils.register_classes_factory(classes)
 
